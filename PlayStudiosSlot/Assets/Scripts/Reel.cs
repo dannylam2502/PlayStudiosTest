fc05c3ee4d3cae29dce8384a268aa5cb45a3a44e
tklqd2502 <dlam@subdreamstudios.com>
2021-11-26 08:53:55 -0800

    - Reel spinning mechanic

---
+++ PlayStudiosSlot/Assets/Scripts/Reel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reel : MonoBehaviour
+{
+    [SerializeField]
+    private List<Slot> _slots;
+
+    [Header("The position the slot will reset")]
+    public Vector3 topPosition;
+
+    public GameObject bottomCollider;
+
+    [SerializeField]
+    private Vector3 _slotVelocity;
+
+    public delegate void OnSlotCollisionWithBottomCallback(Slot slot);
+
+    // The current slot on the top of the reel
+    private Slot _topSlot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string tag = bottomCollider.tag;
+        // set up callback and some data
+        foreach (Slot slot in _slots)
+        {
+            slot.SetOnCollisionBottomCallback(OnSlotCollisionBottom);
+            slot.SetCollisionTag(tag);
+        }
+        _topSlot = _slots[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = _slotVelocity * Time.deltaTime;
+        foreach (Slot slot in _slots)
+        {
+            slot.Move(distance);
+        }
+    }
+
+    public void OnSlotCollisionBottom(Slot slot)
+    {
+        // reset the position to be on top of the current top slot
+        // assign new top slot to this slot
+        slot.ResetToPosition(_topSlot.transform.position + new Vector3(0.0f, 2.5f, 0.0f));
+        _topSlot = slot;
+    }
+}
---
+++ PlayStudiosSlot/Assets/Scripts/Slot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public const float SPEED = 5.0f;
+    private Vector3 _velocity;
+    private Reel.OnSlotCollisionWithBottomCallback _onSlotCollisionWithBottomCallback;
+    private string _collisionTag; // The tag that which handles the collision
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 distance)
+    {
+        transform.Translate(distance);
+    }
+
+    public void ResetToPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    public void SetOnCollisionBottomCallback(Reel.OnSlotCollisionWithBottomCallback callback)
+    {
+        _onSlotCollisionWithBottomCallback = callback;
+    }
+
+    public void SetCollisionTag(string tag)
+    {
+        _collisionTag = tag;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(_collisionTag))
+        {
+            _onSlotCollisionWithBottomCallback?.Invoke(this);
+        }
+    }
+}
